aa5feb51cb678e385288c2abd7e1b1208a8f501e
vishal1997 <mailtovishal@tatanagar.com>
2017-02-05 22:38:59 +0530

    Linear search in linked list

---
+++ linear_search.c
@@ -0,0 +1,17 @@
+void search(struct Node *head, int data)
+{
+	struct Node *ptr=head;
+	int position=0;
+	while(ptr!=NULL && ptr->data!=data)
+	{
+		position++;
+		ptr=ptr->next;
+	}
+	if(ptr->data==data)
+	{
+		printf("\nData found at %d",position);
+	}
+	else if(ptr==NULL)
+		printf("\nData Not found");
+    return;
+}
