270ab340b044503e5a99af0433140850e34f7c90
vishal1997 <mailtovishal@tatanagar.com>
2017-02-05 22:33:57 +0530

    Back traverse single linked list

---
+++ back_traverse.c
@@ -0,0 +1,9 @@
+struct Node *traverse_backward(struct Node *head)
+{
+	struct Node *ptr=head,*cur=NULL;
+	if(head==NULL)
+        return head;
+	cur=traverse_backward(ptr->next);
+	printf("%d",cur->data);
+	return head;
+}
