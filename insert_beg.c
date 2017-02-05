4ed615ec4e7a8d3b5b7bd4bf7b480ec220165f2c
vishal1997 <mailtovishal@tatanagar.com>
2017-02-05 22:38:26 +0530

    insert  at then beg of the linked list

---
+++ insert_beg.c
@@ -0,0 +1,9 @@
+struct Node *insert_at_beg(struct Node *head, int data)
+{
+	struct Node *new_node=(struct Node *)malloc(sizeof(struct Node));
+	struct Node *ptr=head;
+	new_node->data=data;
+	new_node->next=ptr;
+	head=new_node;
+	return head;
+}
