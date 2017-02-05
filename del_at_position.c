78620593a60efcc8d7295e56574ce5e2aadd418f
vishal1997 <mailtovishal@tatanagar.com>
2017-02-05 22:35:46 +0530

    delete node at specific position in linked list

---
+++ del_at_position.c
@@ -0,0 +1,25 @@
+struct Node *del_at_position(struct Node *head, int position)
+{
+	int count=0;
+	struct Node *ptr=head, *prev=NULL;
+	if(position==1)
+	{
+		head=ptr->next;
+		free(ptr);
+		return head;
+	}
+	while(ptr!=NULL && count<position)
+	{
+		prev=ptr;
+		ptr=ptr->next;
+		count++;
+ 	}
+	if(ptr==NULL)
+	{
+		printf("\nPosition Not found");
+		return head;
+	}
+	prev->next=ptr->next;
+	free(ptr);
+	return head;
+}
