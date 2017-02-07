89ba5b3efa3dc20c5662598c4d0fd2c092b1401d
vishal1997 <mailtovishal@tatanagar.com>
2017-02-07 19:47:13 +0530

    condition added empty list

--- del_at_beg.c
+++ del_at_beg.c
@@ -1,6 +1,11 @@
 struct Node *del_at_beg(struct Node *head)
 {
 	struct Node *ptr=head;
+	if(head==NULL)
+    {
+        printf("List is empty");
+        return head;
+    }
 	head=ptr->next;
 	free(ptr);
 	return head;
