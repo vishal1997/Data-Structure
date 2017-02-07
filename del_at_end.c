5d8b21df5c6c6ddde4e1f1fa8b1f57f8f1bd433a
vishal1997 <mailtovishal@tatanagar.com>
2017-02-07 19:48:44 +0530

    set null to end of the list

--- del_at_end.c
+++ del_at_end.c
@@ -1,10 +1,12 @@
 struct Node *del_at_end(struct Node *head)
 {
-	struct Node *ptr=head;
+	struct Node *ptr=head, *temp=NULL;
 	while(ptr->next!=NULL)
 	{
+	    temp=ptr;
 		ptr=ptr->next;
 	}
+	temp->next=NULL;
 	free(ptr);
 	return head;
 }
