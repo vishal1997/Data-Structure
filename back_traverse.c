f321c6fae2142dbab2879971490ef283818e10f0
vishal1997 <mailtovishal@tatanagar.com>
2017-02-07 19:57:25 +0530

    removed copy of head

--- back_traverse.c
+++ back_traverse.c
@@ -1,9 +1,8 @@
 struct Node *traverse_backward(struct Node *head)
 {
-	struct Node *ptr=head,*cur=NULL;
 	if(head==NULL)
         return head;
-	cur=traverse_backward(ptr->next);
-	printf("%d",cur->data);
+	traverse_backward(head->next);
+	printf("%d",head->data);
 	return head;
 }
